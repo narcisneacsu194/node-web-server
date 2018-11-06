c8a8bc132293cceac54ebccfc18b3832a973c831
narcisneacsu194 <narcisneacsu194@gmail.com>
2018-11-07 00:41:35 +0200

    Added projects page.

--- server.js
+++ server.js
@@ -47,6 +47,12 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    text: 'This is a Github link.'
+  });
+});
+
 app.get('/bad', (req, res) => {
   res.send({
     errorMessage: 'Could not handle the request.'
